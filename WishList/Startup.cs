2ded93077e1aa72aa27338c279d7d72f44b74370
BennieGamali <bengodsan@hotmail.com>
2019-06-20 19:50:18 +0200

    git commit -am 'Completed the module'

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using WishList.Data;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace WishList
@@ -17,6 +18,7 @@ namespace WishList
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("WishList"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
